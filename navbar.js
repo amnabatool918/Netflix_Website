1e4c3b25e38ff3a3d816d32dae09915f86f99114
Amna Batool <amnabatool918@gmail.com>
2023-09-30 03:26:24 +0300

    first commit

---
+++ navbar.js
@@ -0,0 +1,10 @@
+function searchon()
+{
+    document.getElementById("search").style.display = "inline-block";
+    document.getElementById("btnsearch").style.display = "none";
+}
+function searchoff()
+{
+    document.getElementById("search").style.display = "none";
+    document.getElementById("btnsearch").style.display = "inline-block";
+}
